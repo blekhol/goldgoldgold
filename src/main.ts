a93f3a5897e7abb30fa30b051059553858ea2da4
Petrik Diak <diak@petrik.hu>
2026-04-24 13:46:35 +0200

    befizetés kezdet

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,7 @@ import 'bootstrap';
 import './style.css';
 
 const link = "https://api-generator.retool.com/U2ra8a/data";
+
+document.addEventListener("DOMContentLoaded", ()=> {
+    document.getElementById("BefizetesBtn")?.addEventListener("click", ()=)
+})
